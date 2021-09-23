fbd075df50c6452bec56e5c6e5fec9f6b012d6b9
san234751 <sankitkundu96@gmail.com>
2021-09-23 21:00:57 +0530

    new changes

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
 const express=require("express");
+const path=require("path");
+const port=process.env.port || 3000;
 app=express();
+const path_main=path.join(__dirname+"../public");
+app.use(express.static(path_main));
+app.set("view engine","ejs");
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+app.listen(port,()=>{
+    console.log("server running");
+});
