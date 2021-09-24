4b351c7ffc92688b2818bc2f415fd9fe80fe2c3f
SANKIT KUNDU <77527865+san234751@users.noreply.github.com>
2021-09-24 23:02:47 +0530

    all the navbar are ready

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const express=require("express");
 const path=require("path");
 const port=process.env.port || 8000;
 app=express();
-const path_main=path.join(__dirname+"../public");
-app.use(express.static(path_main));
+const path_main=path.join(__dirname,'../public');
 app.set("view engine","ejs");
+app.use(express.static(path_main));
 app.get("/",(req,res)=>{
     res.render("index");
 })
